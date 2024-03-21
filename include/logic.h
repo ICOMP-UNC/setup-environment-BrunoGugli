40b4d71d055ff47690bada7fc1bcd5e25d989a5e
Bruno Guglielmotti <brunoguglielmotti@gmail.com>
2024-03-21 15:11:58 -0300

    - Add all files&dir

---
+++ include/logic.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/**
+* @brief Add two numbers
+ *
+ * @param fn First number
+ * @param sn Second number
+ * @return fn + sn
+*/
+int add(int fn, int sn);
+
+/**
+* @brief Substract two numbers
+ *
+ * @param fn First number
+ * @param sn Second number
+ * @return fn - sn
+*/
+int substract(int fn, int sn);
+
+/**
+* @brief Multiply two numbers
+ *
+ * @param fn First number
+ * @param sn Second number
+ * @return fn * sn
+*/
+int multiply(int fn, int sn);
+
+/**
+* @brief Divide two numbers
+ *
+ * @param fn First number
+ * @param sn Second number
+ * @return fn / sn, iff sn != 0
+*/
+double divide(int fn, int sn);
